4dfe290a627468941a010206f8852a722c8da3ce
Jordan Hudgens <jordan@devcamp.com>
2017-06-28 17:33:37 +0000

    Integrated Nested attributes for portfolio model

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,15 @@ end
 
 puts "10 blog posts created"
 
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+    )
+end
+
+puts "5 skills created"  
+
 8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
