b7b6551e2b8c24d61b136b4b326cf16116c87c40
Jordan Hudgens <jordan@devcamp.com>
2017-06-29 15:52:07 +0000

    Customized registration, login, and logout

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -114,7 +114,7 @@ Devise.setup do |config|
   # config.send_email_changed_notification = false
 
   # Send a notification email when the user's password is changed.
-  # config.send_password_change_notification = false
+  config.send_password_change_notification = false
 
   # ==> Configuration for :confirmable
   # A period that the user is allowed to access the website even without
@@ -167,7 +167,7 @@ Devise.setup do |config|
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
-  # config.timeout_in = 30.minutes
+  config.timeout_in = 1.minutes
 
   # ==> Configuration for :lockable
   # Defines which strategy will be used to lock an account.
