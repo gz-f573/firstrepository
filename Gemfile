5e0558d2f392720bcf12894bd9900980a4a53d25
Jordan Hudgens <jordan@devcamp.com>
2017-07-19 14:18:25 +0000

    Implemented partials for blog, font awesome, and pagination

--- Gemfile
+++ Gemfile
@@ -59,3 +59,5 @@ gem 'bootstrap', '~> 4.0.0.alpha6'
 
 gem 'devcamp_view_tool', git: 'https://github.com/gz-f573/devcamp_view_tool'
 gem 'petergate', '~> 1.7', '>= 1.7.5'
+gem 'font-awesome-rails', '~> 4.7', '>= 4.7.0.2'
+gem 'kaminari', '~> 1.0', '>= 1.0.1'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,6 +17,6 @@ module ApplicationHelper
   end
   
   def copyright_generator
-    DevcampViewTool::Renderer.copyright 'Just a random person', 'All rights reserved'
+    DevcampViewTool::Renderer.copyright 'Just a random person', '| All rights reserved'
   end
 end
